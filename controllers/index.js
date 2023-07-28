7398189effc2440425727df1e6e603b2b9d12b09
OronF <oron.fink@gmail.com>
2023-07-29 00:53:22 +0300

    conncting the home page to the app

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
+const path = require('path');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  //res.render('index', { title: 'Express' });
+
+  res.sendFile(path.join(__dirname, '../public', '/webPage.html'));
 });
 
 module.exports = router;
