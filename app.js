97f5059be1b0c410cec352c592e5b5a583a720a4
OronF <oron.fink@gmail.com>
2023-07-29 00:50:45 +0300

    creating the controller of the module category and conncting it to the app

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./controllers/index');
+var usersRouter = require('./controllers/users');
+var categoryController = require('./controllers/category');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api/category', categoryController);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
