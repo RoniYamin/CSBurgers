088025568679b39565696a5a048068c2206bf736
OronF <oron.fink@gmail.com>
2023-08-01 21:15:10 +0300

    creating the route of catgory

--- routes/category.js
+++ routes/category.js
@@ -1,9 +1,19 @@
 var express = require('express'); // Loads express
-const categoryModule = require('../modules/category'); // Loads the module
+const categoryModule = require('../controllers/category'); // Loads the module
 var router = express.Router(); // concting the file to express
 
+router.route('/')
+    .get(categoryModule.getAllCategories)
+    .post(categoryModule.createCategory)
+
+router.route('/:id')
+    .put(categoryModule.updateCategory)
+    .delete(categoryModule.deleteCategory)
+
+module.exports = router;
+
 /* GET Categories listing. */
-router.get('/', function(req, res, next) {
+/*router.get('/', function(req, res, next) {
     let Categories;
     const id = req.query.id; // takes the value from a query
     const name = req.query.name; // takes the value from a query
@@ -15,10 +25,10 @@ router.get('/', function(req, res, next) {
     }
 
     res.end(JSON.stringify(categories)); // printing the array
-});
+});*/
 
 /* POST - create category */
-router.post('/', function(req, res, next) {
+/*router.post('/', function(req, res, next) {
     // gets a json object and takes the value of the name from it
     const category = { 
         name:  req.body.name
@@ -27,10 +37,10 @@ router.post('/', function(req, res, next) {
     const id = categoryModule.create(category); // creating the object in the array
 
     res.end(JSON.stringify(id));
-});
+});*/
 
 /*PUT - update category*/
-router.put('/', function(req, res, next) {
+/*router.put('/', function(req, res, next) {
     // gets a json object and takes the value of the id and the name from it
     const category = {
         id: req.body.id,
@@ -40,16 +50,16 @@ router.put('/', function(req, res, next) {
     categoryModule.update(category); // updating the object with the same id
 
     res.end();
-});
+});*/
 
 /*DELETE - delete category*/
-router.delete('/', function(req, res, next) {
+/*router.delete('/', function(req, res, next) {
     // gets a json object and takes the value of id from it
     const id = req.body.id;
 
     categoryModule.delete(id); // deleting the object with the same id
 
     res.end();
-});
+});*/
 
-module.exports = router;
+//module.exports = router;
