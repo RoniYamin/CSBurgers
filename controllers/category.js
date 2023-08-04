1bc124533a88d3838b0ec67826ba636fdfa05b60
OronF <oron.fink@gmail.com>
2023-08-04 17:10:52 +0300

    adding the controller of category

--- controllers/category.js
+++ controllers/category.js
@@ -1,4 +1,4 @@
-const Categories = [{
+/*const Categories = [{
     id: 1,
     name: 'Starters'
 }];
@@ -80,4 +80,63 @@ module.exports = {
     delete: deleteCatrgory,
     update: updateCatrgory,
     search: searchCatrgories
+}*/
+
+const CatrgoryService = require('../services/category');
+
+const getAllCategories = async (req,res) => {
+    const Catrgories = await CatrgoryService.getAll();
+    res.json(Catrgories);
+}
+
+const createCategory = async (req,res) => {
+    const newCatrgory = await CatrgoryService.create(req.body.name);
+    res.json(newCatrgory);
+}
+
+const updateCategory = async (req,res) => {
+    if (!req.body.name) {
+        res.status(400).json({message:'The new name to the category is required'});
+    }
+
+    const newCatrgory = {
+        id: req.body.id,
+        name: req.body.name
+    }
+
+    const category = await articleService.update(newCatrgory);
+    if (!category) {
+        return res.status(404).json({errors:['Article not found']});
+    }
+
+    res.json(article);
+};
+
+
+const deleteCategory = async (req,res) => {
+    const category = await CatrgoryService.delete(req.params.id);
+    
+    if (!category) {
+        return res.status(404).json({errors:['Category not found']});
+    }
+
+    res.send();
+}
+
+const sreachCategory = async (req,res) => {
+    const category = await articleService.search(req.params.id);
+    
+    if (!category) {
+      return res.status(404).json({errors:['Category not found']});
+    }
+
+    res.json(category);
+}
+
+module.exports = {
+    getAllCategories,
+    createCategory,
+    updateCategory,
+    deleteCategory,
+    sreachCategory
 }
