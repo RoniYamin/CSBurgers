33c782ac964ffcdacb7f959f34c3cee5af4213a7
AmitOvits <toniovits@gmail.com>
2023-08-05 22:52:22 +0300

    adding the 'finishing touch' the service of meals

---
+++ services/meals.js
@@ -0,0 +1,54 @@
+const Meal = require('../models/meals');
+
+const getAll = async() => {
+    return await Meal.find({});
+}
+
+const createMeal = async (name, price, dishes) => {
+    const meal = new Meal({
+        name: name,
+        price: price,
+        dishes: dishes
+    });
+
+    return await meal.save();
+}
+
+const searchMeal = async(id) => {
+    return await Meal.findById(id);
+}
+
+const deleteMeal = async (id) => {
+    const meal = await searchMeal(id);
+
+    if (!meal) {
+        return null;
+    }
+
+    await meal.deleteOne();
+
+    return meal;
+}
+
+const updateMeal = async (newMeal) => {
+    const meal = await searchMeal(newMeal.id);
+
+    if (!meal) {
+        return null;
+    }
+
+    meal.name = newMeal.name;
+    meal.price = newMeal.price;
+    meal.dishes = newMeal.dishes;
+
+    await meal.save()
+    return meal;
+}
+
+module.exports = {
+    getAll,
+    create: createMeal,
+    delete: deleteMeal,
+    update: updateMeal,
+    search: searchMeal
+}
