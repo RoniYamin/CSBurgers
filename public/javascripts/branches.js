73ebb4d2bf1e56e1a13a70a5712e41db82f5717b
OronF <oron.fink@gmail.com>
2023-08-08 13:30:17 +0300

    adding the data to the plus button and styled it

--- public/javascripts/branches.js
+++ public/javascripts/branches.js
@@ -5,18 +5,20 @@ $(document).ready(function() {
     }).done(function(data) {
         data.forEach(function(branch) {
             $('.branches-list').append(`<li class="${branch._id}">
-            <div class="location">
-                <div class="location-icon">
-                    <i class="bi bi-geo-alt-fill"></i>
+                <div class="location-Section"> 
+                    <div class="location">
+                        <div class="location-icon">
+                            <i class="bi bi-geo-alt-fill"></i>
+                        </div>
+
+                        <span class="nameOfLocation">${branch.name}</span>
+                    </div>
+
+                    <div class="plus-icon" data-branch-id="${branch._id}">
+                        <i class="bi bi-plus-circle-fill"></i>
+                    </div>
                 </div>
-
-                <span class="nameOfLocation">${branch.name}</span>
-            </div>
-
-            <div class="plus-icon" data-branch-id="${branch._id}">
-                <i class="bi bi-plus-circle-fill"></i>
-            </div>
-        </li>`);
+            </li>`);
         });
 
         $('.plus-icon').on('click', function() {
@@ -27,7 +29,11 @@ $(document).ready(function() {
                 method: "GET"
             }).done(function(data) {
                 const li = $(`.${id}`);
-                li.append(`<div>
+                li.append(`<div class="location-data">
+                    <span class="data">כתובת: ${data.address}</span>
+                    <span class="data">משלוחים: כן</span>
+                    <span class="data">${data.phoneNumber} :טלפון</span>
+                    <span class="data">שעות פתיחה: ${data.activityTime}</span>
                 </div>`);
             })
         });
