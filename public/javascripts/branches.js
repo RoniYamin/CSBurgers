0328dd2e48985f737f84bb460c7f1be8b5123906
OronF <oron.fink@gmail.com>
2023-08-08 12:29:41 +0300

    adding the event click for the data

--- public/javascripts/branches.js
+++ public/javascripts/branches.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
         method: "GET"
     }).done(function(data) {
         data.forEach(function(branch) {
-            $('.branches-list').append(`<li>
+            $('.branches-list').append(`<li class="${branch._id}">
             <div class="location">
                 <div class="location-icon">
                     <i class="bi bi-geo-alt-fill"></i>
@@ -13,10 +13,25 @@ $(document).ready(function() {
                 <span class="nameOfLocation">${branch.name}</span>
             </div>
 
-            <div class="plus-icon">
+            <div class="plus-icon" data-branch-id="${branch._id}">
                 <i class="bi bi-plus-circle-fill"></i>
             </div>
         </li>`);
         });
+
+        $('.plus-icon').on('click', function() {
+            const btn = $(this);
+            const id = btn.attr('data-branch-id');
+            $.ajax({
+                url: `/api/branches/${id}`,
+                method: "GET"
+            }).done(function(data) {
+                const li = $(`.${id}`);
+                li.append(`<div>
+                </div>`);
+            })
+        });
     }); 
+
+   
 });
