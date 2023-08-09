6988d7fb0b8c940b14873dc4869762e9d55b7c46
OronF <oron.fink@gmail.com>
2023-08-09 16:44:05 +0300

    adding the option to create users from our website

---
+++ public/javascripts/signUp.js
@@ -0,0 +1,72 @@
+$(document).ready(function() {
+
+    const approve = $('#approveBtn');
+
+    approve.on('click', function() {
+
+        const fnameTxt = $('#form3Example1n');
+        const lnameTxt = $('#form3Example1m');
+        const phoneNumberTxt = $('#phone-number');
+        const passwordTxt = $('#form3Example4cg');
+        const passwordApproveTxt = $('#form3Example4cdg');
+
+        const fnameVal = fnameTxt.val();
+        const lnameVal = lnameTxt.val();
+        const phoneNumberVal = phoneNumberTxt.val();
+        const passwordVal = passwordTxt.val();
+        const passwordApproveVal = passwordApproveTxt.val();
+
+        if (fnameVal.length >= 12) {
+            for (let i = 0; i< fnameVal; i++) {
+                if (fnameVal[i] > 'z' || fnameVal[i] < 'A') {
+                    return;
+                }
+            }
+        } else {
+            return;
+        }
+
+        if (lnameVal.length >= 12) {
+            for (let i = 0; i< lnameVal; i++) {
+                if (lnameVal[i] > 'z' || lnameVal[i] < 'A') {
+                    return;
+                }
+            }
+        } else {
+            return;
+        }
+
+        if (phoneNumberVal.length != 9) {
+            return;
+        }
+
+        if (passwordVal.length < 8) {
+            return;
+        }
+
+        if (passwordApproveVal !== passwordVal) {
+            return;
+        }
+
+        $.ajax({
+            url:"/api/user",
+            method: "POST",
+            dataType: "json",
+            contentType: 'application/json',
+            data: JSON.stringify({
+                fname: fnameVal,
+                lname: lnameVal,
+                orders: [],
+                phoneNumber: phoneNumberVal,
+                password: passwordVal,
+                is_Manager: false
+            }),
+            success: function(response) {
+                console.log("Data saved successfully:", response);
+            },
+            error: function(error) {
+                console.error("Error saving data:", error);
+            }
+        });
+    });
+});
