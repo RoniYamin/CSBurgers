e14f8bbe876c6749c0326b44de8a36527ee08b81
OronF <oron.fink@gmail.com>
2023-08-11 14:46:06 +0300

    adding the ajax function for the connction to the website

---
+++ public/javascripts/homePage.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+    const btnSubmit = $('.submit-btn');
+
+    btnSubmit.on('click', function() {
+        const fnameTxt = $('#fnameTxt');
+        const lnameTxt = $('#lnameTxt');
+        const passwordTxt = $('#passwordTxt');
+        
+        const fnameVal = fnameTxt.val();
+        const lnameVal = lnameTxt.val();
+        const passwordVal = passwordTxt.val();
+
+        $.ajax({
+            url:"/api/user",
+            method: "GET",
+            dataType: "json",
+            contentType: 'application/json',
+            data: {
+                fname: fnameVal,
+                lname: lnameVal,
+                password: passwordVal
+            },
+            success: function(response) {
+                console.log("Data saved successfully:", response);
+            },
+            error: function(error) {
+                console.error("Error saving data:", error);
+            }
+        });
+    });
+
+    
+});
